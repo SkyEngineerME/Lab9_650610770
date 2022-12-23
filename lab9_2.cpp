da658172f4349aa7ac59c97ecc0ca3e191dbc1cd
THANATHORN TAEWATTANAPANIT <thanathorn_ta@cmu.ac.th>
2022-12-23 15:05:13 +0700

    Lab9_2Done

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,7 +3,27 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+
+char findGrade(double G){
+ 
+ if(G > 90){
+    return 'A';
+ }
+ else if(G <=  90 && G > 75){
+    return 'B';
+ }
+ else if(G <= 75 && G > 60){
+    return 'C';
+ }
+ else if(G <= 60 && G > 45){
+    return 'D';
+ }
+ else{
+    return 'F';
+ }
+
+
+}
 
 int main(){
 	//Input the number of students
@@ -17,9 +37,9 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
