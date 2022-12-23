3869ae0a6f47b4d75985a920cc5aae1158259691
THANATHORN TAEWATTANAPANIT <thanathorn_ta@cmu.ac.th>
2022-12-23 15:43:29 +0700

    Lab9_3Done

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age,hei,bou;
+    string name;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age < 26){
+     cout << "Enter your height: ";
+     cin >> hei;
+     if(hei < 100 ){
+       name = "Chopper";
+     }
+     else if(hei < 180 && hei >= 100){
+        name = "Usopp";
+     }
+     else{
+          cout << "Enter your bounty: ";
+          cin >> bou;
+          if(bou > 1100000000){
+          name = "Zoro";
+         }
+          else{
+            name = "Sanji";
+         }
+        }
+    }
+    else if(age > 25 && age < 61){
+         cout << "Enter your bounty: ";
+         cin >> bou;
+         if(bou > 500000000){
+            name = "Jinbe";
+         }
+         else{
+            name = "Franky";
+         }
+    }
+    else if(age > 60){
+         name = "Brook";
+    }
+
+  cout << "Your character = "<<name;
+  return 0;
+}
+
+
+
+
+
+
+
 /*
 "Enter your age: "
 "Enter your height: "
